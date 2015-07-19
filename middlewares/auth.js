13157b28b0e56a63e304e58da94e06fa6ea93021
thinkingmik <thinkingmik@debian.dev>
2015-07-19 08:39:51 +0200

    updated auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,43 +6,24 @@ var models  = require('../models');
 
 passport.use(new BasicStrategy(
     function(username, password, callback) {
-        // call sequelize User model and get credentials
+        // get user from db
         models.User.findOne({
             where: {
                 username: username,
                 password: password
             }
         }).then(function(user) {
-            // No user found with that username
+            // no user found with that username and password
             if (!user) {
                 return callback(null, false);
             }
 
-            // Success
+            // success
             return callback(null, user);
 
         }).catch(function(err) {
             return callback(err);
         });
-        /*
-        User.findOne({ username: username }, function (err, user) {
-          if (err) { return callback(err); }
-
-          // No user found with that username
-          if (!user) { return callback(null, false); }
-
-          // Make sure the password is correct
-          user.verifyPassword(password, function(err, isMatch) {
-            if (err) { return callback(err); }
-
-            // Password did not match
-            if (!isMatch) { return callback(null, false); }
-
-            // Success
-            return callback(null, user);
-          });
-        });
-        */
     }
 ));
 
